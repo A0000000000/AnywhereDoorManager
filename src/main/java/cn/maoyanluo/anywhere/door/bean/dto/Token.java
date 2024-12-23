1d17531c4fab6228675723194ccd421419861009
maoyanluo <mengxin_zzl@outlook.com>
2024-12-24 00:55:44 +0800

    字段格式化格式

--- src/main/java/cn/maoyanluo/anywhere/door/bean/dto/Token.java
+++ src/main/java/cn/maoyanluo/anywhere/door/bean/dto/Token.java
@@ -1,5 +1,6 @@
 package cn.maoyanluo.anywhere.door.bean.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class Token {
 
     private String token;
+    @JsonProperty("flush_token")
     private String flushToken;
 
 }
